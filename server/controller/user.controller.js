ad259f045fa446a7bb02b0dd0a8cb2290b1d2fd4
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-08-14 22:35:41 +0530

    Setting cookies for cross site

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -17,57 +17,61 @@ const userSignUp = async (req, res) => {
 
 const userLogin = async (req, res) => {
     const { username, password } = req.body;
-    try{
-        const getUserData = await UserModel.findOne({username}).select("+password");
-        if(getUserData && getUserData.username){
+    try {
+        const getUserData = await UserModel.findOne({ username }).select("+password");
+        if (getUserData && getUserData.username) {
             const result = await bcrypt.compare(password, getUserData.password);
-            if(result){
+            if (result) {
 
                 const token = await getUserData.jwtToken();
                 const cookieOption = {
-                    maxAge: 24*60*60*1000,
-                    httpOnly:true
+                    maxAge: 24 * 60 * 60 * 1000,
+                    httpOnly: true,
+                    sameSite: 'None',
+                    secure: true, // Only set this for HTTPS connections
+                    // For cookies not used in cross-site requests
+                    // sameSite: 'Strict' or 'Lax'
                 };
-                res.cookie("token",token,cookieOption);
+                res.cookie("token", token, cookieOption);
                 res.status(200).json({
-                    success:true,
-                    data:getUserData
+                    success: true,
+                    data: getUserData
                 })
-            }else{
+            } else {
                 res.status(404).json({
-                    success:false,
-                    msg:"Password is in correct, Try Again!"
+                    success: false,
+                    msg: "Password is in correct, Try Again!"
                 })
             }
-        }else{
+        } else {
             res.status(404).json({
-                success:false,
-                msg:"No Account Found Associated with this username"
+                success: false,
+                msg: "No Account Found Associated with this username"
             })
         }
-    } catch(err) {
+    } catch (err) {
         res.status(501).json({
-            msg:err.message
+            msg: err.message
         })
     }
 }
 
 const getUserDetails = async (req, res) => {
-    const {id, username} = req.user;
-    console.log(id,username);
-    try{
-        const userData = await UserModel.findOne({username});
+    const { id, username } = req.user;
+    console.log(id, username);
+    try {
+        const userData = await UserModel.findOne({ username });
         res.status(200).json({
-            msg:"Success",
-            data:userData
+            msg: "Success",
+            data: userData
         })
-    }catch(err) {
-        res.status(501).send({ 
-            success:false,
-            msg:err.message
+    } catch (err) {
+        res.status(501).send({
+            success: false,
+            msg: err.message
         });
     }
-} 
+}
 
 module.exports = {
     userSignUp,
